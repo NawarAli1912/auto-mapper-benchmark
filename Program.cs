9ef25af8429c5de2ba4606e080796211a2cc30da
Nawar <111868983+NawarAli1912@users.noreply.github.com>
2023-04-20 10:56:20 +0300

    update results

--- Program.cs
+++ Program.cs
@@ -154,12 +154,12 @@ public class Benchmark
 }
 
 /*
-|              Method |            Mean |        Error |        StdDev |          Median |
-|-------------------- |----------------:|-------------:|--------------:|----------------:|
-| ManualMappingSingle |        16.50 ns |     0.057 ns |      0.160 ns |        16.49 ns |
-|    AutoMapperSingle |     1,404.34 ns |     3.613 ns |     10.248 ns |     1,406.20 ns |
-|   ManualMappingList |    35,925.42 ns |   109.094 ns |    300.478 ns |    36,004.57 ns |
-|      AutoMapperList | 1,220,364.46 ns | 3,584.858 ns |  9,933.624 ns | 1,220,637.11 ns |
-|      ManualViewList |   111,707.04 ns | 5,044.992 ns | 14,555.956 ns |   108,664.64 ns |
-|  AutoMapperViewList |   104,618.12 ns | 2,817.795 ns |  7,947.640 ns |   101,539.36 ns |
+|              Method |            Mean |         Error |         StdDev |          Median |     Gen0 |    Gen1 | Allocated |
+|-------------------- |----------------:|--------------:|---------------:|----------------:|---------:|--------:|----------:|
+| ManualMappingSingle |        27.18 ns |      1.540 ns |       4.393 ns |        26.03 ns |   0.0268 |       - |     168 B |
+|    AutoMapperSingle |     1,740.13 ns |     27.197 ns |      77.595 ns |     1,728.83 ns |   0.1030 |       - |     664 B |
+|   ManualMappingList |    49,815.33 ns |    565.988 ns |   1,605.613 ns |    49,892.40 ns |  29.3579 |  9.7656 |  184696 B |
+|      AutoMapperList | 1,739,535.17 ns | 46,630.333 ns | 133,038.964 ns | 1,695,421.88 ns | 103.5156 | 50.7813 |  660442 B |
+|      ManualViewList |   105,145.40 ns |  1,767.581 ns |   5,099.876 ns |   104,419.47 ns |  23.9258 |  5.8594 |  151344 B |
+|  AutoMapperViewList |   110,377.45 ns |  2,622.411 ns |   7,439.345 ns |   107,863.04 ns |  23.9258 |  5.8594 |  151405 B |
 */
